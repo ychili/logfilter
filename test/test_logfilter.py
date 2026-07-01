06243b5d80f7b2c2b63a58334c7aa4d2f3a462b5
Ethan Montgomery <ethan.montgomery.30@gmail.com>
2026-07-01 18:04:57 -0400

    Fix weird formatting for good
    Black fixed this issue, "Unnecessary line breaks in method call on multiline
    string" <https://github.com/psf/black/issues/256>, in the 2026 edition, but
    this change side-steps the issue.

--- test/test_logfilter.py
+++ test/test_logfilter.py
@@ -98,11 +98,12 @@ class TestMatchSection(unittest.TestCase):
 
 
 class TestKVParse(unittest.TestCase):
-    basic = r"""
+    _BASIC = """
     # Each line in this string is indented, but that shouldn't matter.
     key1="${HOME}"/logs
 
-    """.splitlines(keepends=True)
+    """
+    basic = _BASIC.splitlines(keepends=True)
     trailing = ["key1=value    \n", "key2=value  # This comment is not discarded\n"]
     spacing = ["  NormalKey = No line breaks please\n", "  Key With Spaces = Value\n"]
     punctuation = ["EqualsSign = 2 + 2 = 4\n", "\n"]
